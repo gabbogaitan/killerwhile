c116c56f56b6e055dfa77b9e77ebdb6b2ded9170
gabbogaitan <gabbogaitan@gmail.com>
2026-07-08 21:39:11 -0300

    actualizacion de catalogo

--- catalogo.js
+++ catalogo.js
@@ -1,12 +1,52 @@
 const catalogoProductos = {
     "remeras": [
         {
+            "archivo": "▲ ■ ● ✖.jpg",
+            "titulo": "▲ ■ ● ✖"
+        },
+        {
+            "archivo": "Baby Yoda.jpg",
+            "titulo": "Baby Yoda"
+        },
+        {
+            "archivo": "Charmander Evo.jpg",
+            "titulo": "Charmander Evo"
+        },
+        {
+            "archivo": "DELORIAN CLOCK liq 35.jpg",
+            "titulo": "DELORIAN CLOCK liq 35"
+        },
+        {
+            "archivo": "Dignidad.jpg",
+            "titulo": "Dignidad"
+        },
+        {
+            "archivo": "DuffBeer.jpg",
+            "titulo": "DuffBeer"
+        },
+        {
+            "archivo": "FREEZER EVO.jpg",
+            "titulo": "FREEZER EVO"
+        },
+        {
             "archivo": "gengar_evo.jpg",
             "titulo": "gengar_evo"
         },
         {
+            "archivo": "Goku - 4 Estrellas.png",
+            "titulo": "Goku - 4 Estrellas"
+        },
+        {
+            "archivo": "Homero en Toalla.jpg",
+            "titulo": "Homero en Toalla"
+        },
+        {
             "archivo": "Homero_Pastelito.png",
             "titulo": "Homero_Pastelito"
+        },
+        {
+            "archivo": "Mario Graffity.jpg",
+            "titulo": "Mario Graffity"
         },
         {
             "archivo": "pacman.png",
@@ -17,20 +57,72 @@ const catalogoProductos = {
             "titulo": "remeras"
         },
         {
+            "archivo": "SKELETOR.jpg",
+            "titulo": "SKELETOR"
+        },
+        {
+            "archivo": "SuperSmash Bros.jpg",
+            "titulo": "SuperSmash Bros"
+        },
+        {
+            "archivo": "Tokyo - S236.jpg",
+            "titulo": "Tokyo - S236"
+        },
+        {
+            "archivo": "Vegeta Sayans.jpg",
+            "titulo": "Vegeta Sayans"
+        },
+        {
+            "archivo": "Venom Purple.jpg",
+            "titulo": "Venom Purple"
+        },
+        {
             "archivo": "volver_al_futuro.png",
             "titulo": "volver_al_futuro"
         }
     ],
     "buzos": [
         {
+            "archivo": "BROOKLYN NEW YORK.jpg",
+            "titulo": "BROOKLYN NEW YORK"
+        },
+        {
+            "archivo": "BUZO XD - NEGRO.jpg",
+            "titulo": "BUZO XD - NEGRO"
+        },
+        {
             "archivo": "buzos.png",
             "titulo": "buzos"
+        },
+        {
+            "archivo": "Hoodie Oversize Pesado - Negro.jpg",
+            "titulo": "Hoodie Oversize Pesado - Negro"
+        },
+        {
+            "archivo": "MICHIGAN.jpg",
+            "titulo": "MICHIGAN"
         }
     ],
     "camperas": [
         {
+            "archivo": "campera_nezuko - copia (2).jpg",
+            "titulo": "campera_nezuko - copia (2)"
+        },
+        {
+            "archivo": "campera_nezuko - copia.jpg",
+            "titulo": "campera_nezuko - copia"
+        },
+        {
             "archivo": "campera_nezuko.jpg",
             "titulo": "campera_nezuko"
+        },
+        {
+            "archivo": "campera_one_piece - copia (2).jpg",
+            "titulo": "campera_one_piece - copia (2)"
+        },
+        {
+            "archivo": "campera_one_piece - copia.jpg",
+            "titulo": "campera_one_piece - copia"
         },
         {
             "archivo": "campera_one_piece.jpg",
@@ -39,11 +131,59 @@ const catalogoProductos = {
     ],
     "medias": [
         {
+            "archivo": "medias - copia (2).jpg",
+            "titulo": "medias - copia (2)"
+        },
+        {
+            "archivo": "medias - copia (3).jpg",
+            "titulo": "medias - copia (3)"
+        },
+        {
+            "archivo": "medias - copia (4).jpg",
+            "titulo": "medias - copia (4)"
+        },
+        {
+            "archivo": "medias - copia (5).jpg",
+            "titulo": "medias - copia (5)"
+        },
+        {
+            "archivo": "medias - copia (6).jpg",
+            "titulo": "medias - copia (6)"
+        },
+        {
+            "archivo": "medias - copia.jpg",
+            "titulo": "medias - copia"
+        },
+        {
             "archivo": "medias.jpg",
             "titulo": "medias"
         }
     ],
     "pantalones": [
+        {
+            "archivo": "pantalones - copia (2).jpg",
+            "titulo": "pantalones - copia (2)"
+        },
+        {
+            "archivo": "pantalones - copia (3).jpg",
+            "titulo": "pantalones - copia (3)"
+        },
+        {
+            "archivo": "pantalones - copia (4).jpg",
+            "titulo": "pantalones - copia (4)"
+        },
+        {
+            "archivo": "pantalones - copia (5).jpg",
+            "titulo": "pantalones - copia (5)"
+        },
+        {
+            "archivo": "pantalones - copia (6).jpg",
+            "titulo": "pantalones - copia (6)"
+        },
+        {
+            "archivo": "pantalones - copia.jpg",
+            "titulo": "pantalones - copia"
+        },
         {
             "archivo": "pantalones.jpg",
             "titulo": "pantalones"
